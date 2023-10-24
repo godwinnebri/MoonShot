1d5f7f51609d2eeefaaab586d7c9a84f77f22586
Godwin Ebri <104990430+godwinnebri@users.noreply.github.com>
2023-10-24 23:09:56 +0100

    Initial Commit

---
+++ MoonShot/MoonShotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonShotApp.swift
+//  MoonShot
+//
+//  Created by Godwin IE on 24/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct MoonShotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
