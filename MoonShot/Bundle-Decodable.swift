a52211bce1923297da1399d202b963809ce97ad6
Godwin Ebri <104990430+godwinnebri@users.noreply.github.com>
2023-10-30 21:25:16 +0100

    used generic type to decode mission.json

--- MoonShot/Bundle-Decodable.swift
+++ MoonShot/Bundle-Decodable.swift
@@ -8,7 +8,12 @@
 import Foundation
 
 extension Bundle {
-    func decode(_ file: String) -> [String: Astronaut] {
+    /// `T` Denotes a generic type
+    /// Using `<T:Codable>`means we are letting swift determine the...
+    /// ...type that will be returned and constrain it to the codable protocol
+    /// the returned type was formerly `[String: Astronaut]`
+    
+    func decode<T: Codable> (_ file: String) -> T {
         guard let url = self.url(forResource: file, withExtension: nil) else {
             fatalError("Failed to locate \(file) in bundle")
         }
@@ -19,7 +24,7 @@ extension Bundle {
         
         let decoder = JSONDecoder()
         
-        guard let loaded = try? decoder.decode([String: Astronaut].self, from: data) else {
+        guard let loaded = try? decoder.decode(T.self, from: data) else {
             fatalError("Failed to decode \(file) from bundle")
         }
         
--- MoonShot/ContentView.swift
+++ MoonShot/ContentView.swift
@@ -8,13 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
-    let astronauts = Bundle.main.decode("astronauts.json")
+    let astronauts : [String: Astronaut] = Bundle.main.decode("astronauts.json")
+    let mission: [Mission] = Bundle.main.decode("missions.json")
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("\(astronauts["grissom"]?.description ?? "Grissom")")
+            Text("\(mission[0].launchDate ?? "date of beni")")
+            Text("\(mission[5].launchDate ?? "date of beni")")
+            Text("\(mission[1].crew[2].role)")
+
+
         }
         .padding()
     }
