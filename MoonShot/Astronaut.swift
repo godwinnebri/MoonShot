aff48adccbc1e0a3ef628e7cb6d48e9e7268ba38
Godwin Ebri <104990430+godwinnebri@users.noreply.github.com>
2023-10-30 20:54:14 +0100

    json decode and bundle

---
+++ MoonShot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  MoonShot
+//
+//  Created by Godwin IE on 26/10/2023.
+//
+
+import Foundation
+
+struct Astronaut : Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
